92b14bb21adbfe99093e090d205458c49a8039a9
bruttins <bruttins@gmail.com>
2025-03-02 21:28:35 +0100

    added left and right movement

--- main.py
+++ main.py
@@ -17,10 +17,13 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        dt = clock.tick(60) / 1000
+        player_instance.update(dt)
+
         screen.fill("black")
         player_instance.draw(screen)
         pygame.display.flip()
-        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
